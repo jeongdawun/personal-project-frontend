f18ec34f075772e6aa8690c79155249b4acaf8f6
jeongdawun <jeongdawun33@gmail.com>
2023-07-03 16:07:57 +0900

    init: [정다운] Vue 초기 설정 [VHC-1]

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <div>
+      <h2>메인이다</h2>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+
+    components: {
+    },
+  }
+</script>
