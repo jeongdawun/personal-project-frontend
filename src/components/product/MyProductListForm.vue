b6dda4f37037606093ddd4eee9b8b7af43f932ea
jeongdawun <jeongdawun33@gmail.com>
2023-07-19 11:26:28 +0900

    style: [정다운] 나의 상품 목록 페이지 스타일 적용 [VHC-28]

--- src/components/product/MyProductListForm.vue
+++ src/components/product/MyProductListForm.vue
@@ -2,61 +2,63 @@
     <v-container class="container">
         <v-expansion-panels
             v-model="panel"
+            :disabled="disabled"
             multiple
-            focusable>
+            focusable
+            >
         <v-expansion-panel>
-        <v-expansion-panel-header>상품 카테고리</v-expansion-panel-header>
+        <v-expansion-panel-header><span class="headerMenu">상품 정보</span></v-expansion-panel-header>
         <v-expansion-panel-content>
-            <input type="text" :value="product.category" readonly/>
+            <v-text-field :value="product.category" label="카테고리" disabled></v-text-field>
+            <v-text-field :value="product.productName" label="상품명" disabled></v-text-field>
+            <v-textarea :value="product.productDetails" label="상세내용"></v-textarea>
         </v-expansion-panel-content>
         </v-expansion-panel>
 
         <v-expansion-panel>
-        <v-expansion-panel-header>상품명</v-expansion-panel-header>
+        <v-expansion-panel-header><span class="headerMenu">상품 이미지</span></v-expansion-panel-header>
         <v-expansion-panel-content>
-            <input type="text" :value="product.productName" readonly/>
+            <v-row>
+                <v-col>
+                    메인 이미지<v-img width="400" height="auto" :src="product.productMainImage ? getImage(product.productMainImage) : ''"></v-img>
+                </v-col>
+                <v-col v-for="(image, index) in product.productImageNameList" :key="index" cols="12" sm="6" md="4" lg="3" xl="2">
+                    상세 이미지 {{index + 1}}<v-img width="auto" height="200" :src="product.productImageNameList ? getImage(image) : ''"></v-img>
+                </v-col>
+            </v-row>
+            <v-file-input multiple truncate-length="15"></v-file-input>
         </v-expansion-panel-content>
         </v-expansion-panel>
 
         <v-expansion-panel>
-        <v-expansion-panel-header>메인 이미지</v-expansion-panel-header>
-        <v-expansion-panel-content>
-            <v-img :src="product.productMainImage ? getImage(product.productMainImage) : ''"></v-img>
-        </v-expansion-panel-content>
-        </v-expansion-panel>
-
-        <v-expansion-panel>
-        <v-expansion-panel-header>상세 이미지</v-expansion-panel-header>
-        <v-expansion-panel-content v-for="(image, index) in product.productImageNameList" :key="index">
-            <v-img :src="product.productImageNameList ? getImage(image) : ''"></v-img>
-        </v-expansion-panel-content>
-        </v-expansion-panel>
-
-        <v-expansion-panel>
-        <v-expansion-panel-header>상세내용</v-expansion-panel-header>
-        <v-expansion-panel-content>
-            <span>{{ product.productDetails }}</span>
-        </v-expansion-panel-content>
-        </v-expansion-panel>
-
-        <v-expansion-panel>
-        <v-expansion-panel-header>옵션</v-expansion-panel-header>
+        <v-expansion-panel-header><span class="headerMenu">옵션 정보</span></v-expansion-panel-header>
         <v-expansion-panel-content v-for="(productOption, index) in product.productOptionResponseFormList" :key="index">
-            <span>{{ productOption.optionName }}</span>
-            <span>{{ productOption.optionPrice }}</span>
+            <v-text-field v-model="productOption.optionName" label="옵션명"></v-text-field>
+            <v-text-field v-model="productOption.optionPrice" label="옵션 가격"></v-text-field>
         </v-expansion-panel-content>
         </v-expansion-panel>
 
-    <v-expansion-panel>
-        <v-expansion-panel-header>재고</v-expansion-panel-header>
-        <v-expansion-panel-content v-for="(option, index) in product.productOptionWithVacancyResponseFormList" :key="index">
-            <span>{{ option.id }}</span>
-            <span>{{ option.dateList }}</span>
-            <span>{{ option.campsiteVacancyList }}</span>
-        </v-expansion-panel-content>
-        </v-expansion-panel>
-    </v-expansion-panels>
-    <v-btn @click="deleteProduct">삭제하기</v-btn>
+        <v-expansion-panel>
+            <v-expansion-panel-header><span class="headerMenu">재고</span></v-expansion-panel-header>
+            <v-expansion-panel-content v-for="(option, index) in product.productOptionWithVacancyResponseFormList" :key="index">
+                <v-row>
+                    <v-col cols="2">
+                        <v-text-field v-model="option.id" label="옵션번호" readonly></v-text-field>
+                    </v-col>
+                    <v-col cols="5">
+                        <v-text-field v-model="option.dateList" label="날짜"></v-text-field>
+                    </v-col>
+                    <v-col cols="5">
+                        <v-text-field v-model="option.campsiteVacancyList" label="빈자리"></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+
+    <a @click="deleteProduct">
+        <span class="delete">상품 삭제하기 ></span>
+    </a>
     </v-container>
 </template>
 
@@ -76,6 +78,8 @@ export default {
     data () {
         return {
             id: 0,
+            panel: [0],
+            disabled: false,
         }
     },
     methods: {
@@ -89,16 +93,38 @@ export default {
             await this.requestDeleteProductToSpring(this.product.id)
         }
     },
-    async mounted(){
-        await this.getImage()
-    },
 }
 </script>
 
 <style scoped>
 @import "../../assets/styles/fonts.css";
 .container {
-    width: 80%;
+    width: 60%;
     margin-top: 100px;
+}
+.v-expansion-panel-header {
+    color: rgb(76, 129, 32);
+    height: 20px;
+    border-radius: 0;
+    font-family: 'SUIT-Regular';
+    font-weight: 100;
+}
+.v-expansion-panel-content {
+    font-family: 'SUIT-Regular';
+    font-weight: 100;
+    padding-top: 14px
+}
+.delete {
+    font-family: 'SUIT-Regular';
+    font-weight: 200;
+    font-size: 14px;
+    text-decoration: none;
+    color: #535353;
+    display: inline-block;
+    white-space: nowrap;
+    padding: 20px;
+}
+.headerMenu {
+    color: rgb(87, 87, 87);
 }
 </style>
